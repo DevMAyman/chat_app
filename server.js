b244fe7b574cd40d6f9f155caa554aa7c7ed6615
DevMAyman <eng.developer.mohamed.ayman@gmail.com>
2024-05-11 16:45:19 +0300

    chat app

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const io = require("socket.io")(server);
+
+
+
+
+app.set('views','./Views')
+app.use(express.static('Views'))
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+  return res.render('index.html');
+});
+
+io.on("connection", (socket) => {
+    socket.on("chat message",data => {
+        io.sockets.emit("chat message", data);
+    });
+});
+
+server.listen(3000, () => {
+  console.log("listening on *:3000");
+});
